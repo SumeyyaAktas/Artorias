9ef999c9d80710a60a748d63c0d1599f5559d635
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-08 23:41:20 -0500

    integrate pci scan

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -1,5 +1,5 @@
-[org 0x7e00]                   ; mbr loads at 0x7e00                
-[bits 16]                      
+[org 0x7e00]
+[bits 16]
 
 start_stage2:
     mov [boot_drive], dl
@@ -10,49 +10,71 @@ start_stage2:
 
     mov bx, switching_pm_str
     call print16_string
-    call print16_newline  
+    call print16_newline
 
-    mov ah, 0x03               ; get cursor position
+    mov ah, 0x03
     mov bh, 0x00
-    int 0x10            
-    
-    ; offset = (row * 80 + column) * 2
-    xor ax, ax
-    mov al, dh                 ; al = row
-    mov bx, 160                ; 80 chars * 2 bytes per char
-    mul bx                     ; ax = row * 160
-    mov si, ax                 ; si = row offset
+    int 0x10
 
     xor ax, ax
-    mov al, dl                 ; al = column
-    shl ax, 1                  ; ax = column * 2
-    add si, ax                 ; si = final offset
+    mov al, dh
+    mov bx, 160
+    mul bx
+    mov si, ax
 
-    mov [cursor_pos], si       ; save for protected mode printing
+    xor ax, ax
+    mov al, dl
+    shl ax, 1
+    add si, ax
 
-    call switch_to_pm               
+    mov [cursor_pos], si
+
+    call switch_to_pm
 
 [bits 32]
 
-; called by switch_to_pm after mode transition
 begin_pm:
     mov ebx, protected_mode_str
     call print32_string
- 
-    jmp $                    
+    
+    call pci_scan_usb
+    
+    cmp byte [usb_controller_found], 0
+    je .no_usb
+    
+    mov ebx, usb_init_str
+    call print32_string
+    
+    jmp .halt
+    
+.no_usb:
+    mov ebx, usb_failed_str
+    call print32_string
+    
+.halt:
+    jmp $
 
 %include "boot/print16_string.asm"
 %include "boot/print32_string.asm"
 %include "boot/gdt.asm"
 %include "boot/switch_to_pm.asm"
+%include "boot/pci.asm"                
 
 real_mode_str: db 'Running in 16-bit real mode', 0
 switching_pm_str: db 'Switching to protected mode...', 0
-protected_mode_str: db 'Now in 32-bit protected mode', 0
+protected_mode_str: db 'Now in 32-bit protected mode | ', 0
+usb_init_str: db 'USB controller ready for initialization', 0
+usb_failed_str: db 'Cannot proceed without USB controller', 0
 
 boot_drive: db 0
-cursor_pos: dd 0
-cursor_row: db 0
-cursor_col: db 0
 
-times 4096 - ($ - $$) db 0     ; pad to 4kb (8 sectors)
+cursor_pos: dd 0
+
+usb_controller_found: db 0     ; 1 if found
+usb_controller_type: db 0      
+usb_controller_bar0: dd 0      ; mmio base address
+usb_controller_bus: db 0            
+usb_controller_device: db 0            
+usb_controller_function: db 0           
+
+times 4096 - ($ - $$) db 0
