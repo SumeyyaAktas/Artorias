be7d22c0ca4e7a9c74cf1533f657d9a153e59a83
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-13 10:51:58 -0500

    add kernel header

---
+++ kernel/include/kernel.h
@@ -0,0 +1,11 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#define VIDEO_MEMORY 0xB8000
+#define WHITE_ON_BLACK 0x0F
+
+void print(const char* str, int row);
+void clear_screen(void);
+void kernel_main(void);
+
+#endif KERNEL_H
