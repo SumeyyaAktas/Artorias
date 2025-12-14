c78e8d8aad11cceac1237519d79dd67efd1c2e90
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-13 22:41:02 -0500

    define pci class/subclass info

--- kernel/include/pci.h
+++ kernel/include/pci.h
@@ -9,8 +9,11 @@
 #define PCI_VENDOR_ID 0x00
 #define PCI_DEVICE_ID 0x02
 #define PCI_CLASS 0x0B
+#define PCI_CLASS_INFO 0x08
 #define PCI_SUBCLASS 0x0A
 #define PCI_HEADER_TYPE 0x0E
+#define PCI_CLASS_SERIAL 0x0C
+#define PCI_SUBCLASS_USB 0x03
 
 uint32_t pci_read(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 void pci_enumerate(void);
