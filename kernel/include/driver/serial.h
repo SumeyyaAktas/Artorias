3d3cef6030d3b7cb714e33dc938a8fb78298d1e5
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-13 21:23:57 -0500

    add hex8 func

--- kernel/include/driver/serial.h
+++ kernel/include/driver/serial.h
@@ -9,5 +9,6 @@ void serial_init(void);
 void serial_write_char(char c);
 void serial_print(const char* str);
 void serial_print_hex(uint32_t value);
+void serial_print_hex8(uint8_t value);
 
 #endif
