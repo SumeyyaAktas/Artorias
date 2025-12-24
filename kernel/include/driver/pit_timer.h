526077a76d5f7f4ca8d62dcb7459fc2c32d77798
Sumeyya Aktas <celestinedior1225@gmail.com>
2025-12-24 15:20:56 -0500

    use mode 2

--- kernel/include/driver/pit_timer.h
+++ kernel/include/driver/pit_timer.h
@@ -5,7 +5,6 @@
 
 #define PIT_CHANNEL_0 0x40
 #define PIT_COMMAND 0x43
-
 #define PIT_BASE_FREQ 1193182
 
 void pit_wait(uint32_t ms);
--- kernel/src/driver/pit_timer.c
+++ kernel/src/driver/pit_timer.c
@@ -1,15 +1,15 @@
-#include "driver/pit_timer.h"
 #include <ports.h> 
+#include "driver/pit_timer.h"
 
 void pit_wait(uint32_t ms) 
 {
-    uint32_t total_ticks = ms * (PIT_BASE_FREQ / 1000);
+    uint32_t total_ticks = (uint32_t)((uint32_t)ms * PIT_BASE_FREQ / 1000);
 
     while (total_ticks > 0) 
     {
         uint16_t current_chunk = (total_ticks > 0xFFFF) ? 0xFFFF : (uint16_t)total_ticks;
 
-        outb(PIT_COMMAND, 0x30); 
+        outb(PIT_COMMAND, 0x34); 
         outb(PIT_CHANNEL_0, (uint8_t)(current_chunk & 0xFF));
         outb(PIT_CHANNEL_0, (uint8_t)((current_chunk >> 8) & 0xFF));
 
