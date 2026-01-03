c59031dadd0247a9c28165daa835c65f7437caff
Sumeyya Aktas <celestinedior1225@gmail.com>
2026-01-02 20:20:26 -0500

    define tweak

--- boot/mbr.asm
+++ boot/mbr.asm
@@ -3,14 +3,14 @@
 ; compatibility with chain-loading other bootloaders or operating systems 
 ; that strictly expect to be loaded at 0x7C00.
 
-%define load_address 0x7C00            
-%define relocate_address 0x0600
-%define stage2_address 0x7E00
-%define stage2_sectors 8
-%define sector_size 512
-%define offset_reloc (relocate_address - load_address) 
+%define LOAD_ADDRESS 0x7C00            
+%define RELOCATE_ADDRESS 0x0600
+%define STAGE2_ADDRESS 0x7E00
+%define STAGE2_SECTORS 8
+%define SECTOR_SIZE 512
+%define OFFSET_RELOC (RELOCATE_ADDRESS - LOAD_ADDRESS) 
 
-[org load_address]        
+[org LOAD_ADDRESS]        
 [bits 16]                      
 
 start_boot:
@@ -19,7 +19,7 @@ start_boot:
     mov ds, ax                                 
     mov es, ax                                  
     mov ss, ax                              
-    mov sp, load_address          
+    mov sp, LOAD_ADDRESS          
  
     ; dl register contains the boot drive id from BIOS.
     ; We save it immediately before any instructions modify it.
@@ -30,16 +30,16 @@ start_boot:
     ; This is the relocation logic. We do this because moving 
     ; the MBR to 0x0600 prevents memory collisions during 
     ; multi-stage loading.
-    mov si, load_address       
-    mov di, relocate_address   
-    mov cx, sector_size        
+    mov si, LOAD_ADDRESS       
+    mov di, RELOCATE_ADDRESS   
+    mov cx, SECTOR_SIZE        
     cld                        
     rep movsb                  
 
     ; Perform a far jump to the relocated code to update 
     ; the instruction pointer (IP) and ensure 
     ; code segment is synchronized (0x0000).
-    jmp 0x0000:(continue_boot + offset_reloc) 
+    jmp 0x0000:(continue_boot + OFFSET_RELOC) 
 
 continue_boot:
     call clear_screen  
@@ -48,34 +48,34 @@ continue_boot:
     ; is too small for EHCI initialization. We load 8 sectors (4KB) 
     ; immediately following the MBR.
     mov ah, 0x02                         
-    mov al, stage2_sectors          
+    mov al, STAGE2_SECTORS          
     mov ch, 0                ; Cylinder 0                            
     mov cl, 2                ; Sector 2 (Sector 1 is the MBR)             
     mov dh, 0                ; Head 0           
 
-    mov dl, [boot_drive + offset_reloc]  
-    mov bx, stage2_address   ; Destination buffer in memory       
+    mov dl, [boot_drive + OFFSET_RELOC]  
+    mov bx, STAGE2_ADDRESS   ; Destination buffer in memory       
     int 0x13                   
 
     jc disk_error            ; Carry flag set indicates a BIOS disk failure                
-    cmp al, stage2_sectors   ; BIOS returns number of sectors actually read         
+    cmp al, STAGE2_SECTORS   ; BIOS returns number of sectors actually read         
     jne disk_error             
 
-    mov bx, stage2_loaded_str + offset_reloc
+    mov bx, stage2_loaded_str + OFFSET_RELOC
     call print16_string
     call print16_newline  
 
     ; Pass the boot drive ID to Stage 2 in DL
-    mov dl, [boot_drive + offset_reloc]
-    jmp 0x0000:stage2_address                    
+    mov dl, [boot_drive + OFFSET_RELOC]
+    jmp 0x0000:STAGE2_ADDRESS                    
 
 disk_error:
-    mov bx, disk_error_str + offset_reloc
+    mov bx, disk_error_str + OFFSET_RELOC
     call print16_string
     call print16_newline
 
 halt:
-    mov bx, system_halted_str + offset_reloc
+    mov bx, system_halted_str + OFFSET_RELOC
     call print16_string
     call print16_newline
     cli                      ; Do not allow interrupts while halted         
